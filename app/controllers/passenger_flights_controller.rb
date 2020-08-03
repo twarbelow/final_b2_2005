f2705850c333b802d80f5c98f7fc8a3819436778
twarbelow <taija.warbelow@google.com>
2020-08-03 11:00:58 -0700

    create method not populating PasssengerFlight with auto generated id

--- app/controllers/passenger_flights_controller.rb
+++ app/controllers/passenger_flights_controller.rb
@@ -1,5 +1,7 @@
 class PassengerFlightsController < ApplicationController
   def create
     require 'pry'; binding.pry
+    PassengerFlight.create(passenger_id: params[:passenger_id], flight_id: params["Flight Number"])
+    redirect_to "/passegners/#{params[:passenger_id]}"
   end
 end
