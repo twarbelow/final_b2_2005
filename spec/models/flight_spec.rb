4468b09f8a5fa868273a54297dcbd6be2668df6e
twarbelow <taija.warbelow@google.com>
2020-08-03 08:36:22 -0700

    fix relationships syntax

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -12,6 +12,6 @@ describe Flight, type: :model do
   describe "relationships" do
     it { should belong_to :airline }
     it { should have_many :passenger_flights }
-    it { should have_many :passengers, through: :passenger_flights }
+    it { should have_many(:passengers).through(:passenger_flights) }
   end
 end
